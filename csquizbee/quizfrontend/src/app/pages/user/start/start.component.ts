148d5b379c5150fd0c0ffd0a03089e9f44d53936
Ibrahim-Lokman <ibrahimlokman.bd@gmail.com>
2022-12-06 15:10:55 +0600

    Fronted: Attempt quiz and view marks based on correct answer.
    Track of number of attempted.

--- csquizbee/quizfrontend/src/app/pages/user/start/start.component.ts
+++ csquizbee/quizfrontend/src/app/pages/user/start/start.component.ts
@@ -11,6 +11,10 @@ import Swal from 'sweetalert2';
 export class StartComponent implements OnInit {
   qid = 0;
   questions = {} as any;
+  marksGot = 0; 
+  correctAnswer = 0;
+  attempted = 0;
+  isSubmit = false;
   constructor(
     private locationSt: LocationStrategy, 
     private _route: ActivatedRoute,
@@ -28,7 +32,12 @@ export class StartComponent implements OnInit {
     .getQuestionsOfQuizForTest(this.qid)
     .subscribe((data:any) => {
       this.questions = data;
-      console.log(data);
+
+      this.questions.forEach((q:any) => {
+        q['givenAnswer'] = '';
+      });
+
+      console.log(this.questions);
     },
     (error:any) => {
       console.log(error);
@@ -42,6 +51,35 @@ export class StartComponent implements OnInit {
     this.locationSt.onPopState(() => {
       history.pushState('', '', location.href);
     });
+  }
+
+  submitQuiz(){
+    Swal.fire({
+      title: 'Do you want to submit the quiz?',
+      showCancelButton: true,
+      confirmButtonText: 'Submit',
+      icon: 'info',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.isSubmit= true;
+        this.questions.forEach((q:any) => {
+          if(q.givenAnswer == q.answer){
+            this.correctAnswer++;
+            let marksSingle = this.questions[0].quiz.maxMarks / this.questions.length;
+            this.marksGot += marksSingle;
+          }
+
+          if (q.givenAnswer.trim() != ''){
+            this.attempted++;
+          }
+    
+        });
+        console.log('Correct Answers: ' + this.correctAnswer);
+        console.log('Marks Got: '+ this.marksGot);
+        console.log('Attempted: '+ this.attempted);
+      } 
+    });
+
   }
 
 }
